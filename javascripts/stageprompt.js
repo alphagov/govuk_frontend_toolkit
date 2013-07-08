cc6f2a8a5324cbc9136c7bc0672364c93efb3f93
Gareth <grogers@thoughtworks.com>
2013-07-08 11:59:33 +0100

    Update stagepropmpt

--- javascripts/stageprompt.js
+++ javascripts/stageprompt.js
@@ -3,20 +3,26 @@ var GOVUK = GOVUK || {};
 GOVUK.performance = GOVUK.performance || {};
 
 GOVUK.performance.stageprompt = (function () {
-  var setup, setUpForGoogleAnalytics;
+
+  var setup, setupForGoogleAnalytics, splitAction;
+
+  splitAction = function (action) {
+    var parts = action.split(':');
+    if (parts.length <= 3) return parts;
+    return [parts.shift(), parts.shift(), parts.join(':')];
+  };
 
   setup = function (analyticsCallback) {
     var journeyStage = $('[data-journey]').attr('data-journey'),
-        journeyHelpers = $('[data-journey-helper]');
+        journeyHelpers = $('[data-journey-click]');
+
     if (journeyStage) {
-      analyticsCallback(journeyStage);
+      analyticsCallback.apply(null, splitAction(journeyStage));
     }
     
-    if (journeyHelpers.length) {
-      journeyHelpers.on('click', function (event) {
-        analyticsCallback($(this).data('journey-helper'));
-      });
-    }
+    journeyHelpers.on('click', function (event) {
+      analyticsCallback.apply(null, splitAction($(this).data('journey-click')));
+    });
   };
   
   setupForGoogleAnalytics = function () {
@@ -29,6 +35,6 @@ GOVUK.performance.stageprompt = (function () {
   };
 }());
 
-GOVUK.performance.sendGoogleAnalyticsEvent = function (action) {
-  _gaq.push(['_trackEvent', 'stagePrompt', action, undefined, undefined, true]);
+GOVUK.performance.sendGoogleAnalyticsEvent = function (category, event, label) {
+  _gaq.push(['_trackEvent', category, event, label, undefined, true]);
 };
