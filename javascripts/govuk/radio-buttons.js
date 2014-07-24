e8c53415f99dda9b1ba8fad064ed3a9aba3ee09c
tombye <tombaromba@gmail.com>
2014-07-24 22:58:16 +0100

    Add radio-buttons script and tests

---
+++ javascripts/govuk/radio-buttons.js
@@ -0,0 +1,57 @@
+(function () {
+  "use strict"
+  var root = this,
+      $ = root.jQuery;
+
+  if (typeof GOVUK === 'undefined') { root.GOVUK = {}; }
+
+  var RadioButtons = function ($elms) {
+    this.$elms = $elms;
+    this.selectedClass = 'selected';
+    this.setup();
+    this.bindEvents();
+  };
+  RadioButtons.prototype.setup = function () {
+    var _this = this;
+
+    this.selections = {};
+    $.each(this.$elms, function (index, elm) {
+      var $elm = $(elm),
+          radioName = $elm.attr('name');
+
+      if (typeof _this.selections[radioName] === 'undefined') {
+        _this.selections[radioName] = false;
+      }
+      if ($elm.is(':checked')) {
+        _this.markSelected($elm);
+        _this.selections[radioName] = $elm.attr('id');
+      }
+    });
+  };
+  RadioButtons.prototype.bindEvents = function () {
+    var _this = this;
+
+    // some browsers fire the 'click' when the selected radio changes by keyboard
+    this.$elms.on('click change', function (e) {
+      var $elm = $(e.target);
+
+      if ($elm.is(':checked')) {
+        _this.markSelected($elm);
+      }
+    });
+  };
+  RadioButtons.prototype.markSelected = function ($elm) {
+    var radioName = $elm.attr('name'),
+        previousSelection = this.selections[radioName];
+
+    if (previousSelection) {
+      $('#' + previousSelection).parent('label').removeClass(this.selectedClass);
+    }
+    $elm.parent('label').addClass(this.selectedClass);
+    this.selections[radioName] = $elm.attr('id');
+  };
+  RadioButtons.prototype.setSelectedClass = function (className) {
+    this.selectedClass = className;
+  };
+  root.GOVUK.RadioButtons = RadioButtons;
+}).call(this);
---
+++ spec/RadioButtonSpec.js
@@ -0,0 +1,123 @@
+describe("radio-buttons", function () {
+  var $radioButtons,
+      $labels;
+
+  beforeEach(function () {
+    $labels = $(
+        '<label class="selectable">' +
+          'Small' +
+          '<input type="radio" name="size" id="small" value="small" />' +
+        '</label>' +
+        '<label class="selectable">' +
+          'Medium' +
+          '<input type="radio" name="size" id="medium" value="medium" />' +
+        '</label>' +
+        '<label class="selectable">' +
+          'Large' +
+          '<input type="radio" name="size" id="large" value="large" />' +
+        '</label>'
+    );
+    $radioButtons = $labels.find('input');
+    $(document.body).append($labels);
+  });
+
+  afterEach(function () {
+    $labels.remove();
+  });
+
+  it("Should create a new instance with the correct interface", function () {
+    var buttons = new GOVUK.RadioButtons($radioButtons);
+
+    expect(buttons.setup).toBeDefined();
+    expect(buttons.bindEvents).toBeDefined();
+    expect(buttons.markSelected).toBeDefined();
+    expect(buttons.setSelectedClass).toBeDefined();
+  });
+
+  describe("setup method", function () {
+    it("Should mark the label of any checked radios as selected", function () {
+      var radioButtonsMock = {
+            'markSelected' : function () {},
+            '$elms' : $radioButtons
+          };
+
+      $radioButtons.eq(0).attr('checked', true);
+      spyOn(radioButtonsMock, 'markSelected');
+      GOVUK.RadioButtons.prototype.setup.call(radioButtonsMock);
+      expect(radioButtonsMock.markSelected).toHaveBeenCalled();
+    });
+  });
+
+  describe("bindEvents method", function () {
+    it("Should bind click and change events to each radio", function () {
+      var radioButtonsMock = {
+            '$elms' : $radioButtons
+          },
+          eventsBound;
+
+      spyOn($.fn, 'on').andCallFake(function (evt, func) {
+        eventsBound = evt;
+      });
+      expect($.fn.on.calls.length).toEqual(0);
+      GOVUK.RadioButtons.prototype.bindEvents.call(radioButtonsMock);
+      expect($.fn.on).toHaveBeenCalled();
+      expect(eventsBound).toEqual('click change');
+    });
+
+    it("Should call the markSelected method on any checked radio that's the target of an event", function () {
+      var radioButtonsMock = {
+            '$elms' : $radioButtons,
+            'markSelected' : function () {}
+          },
+          eventsBound;
+
+      spyOn($.fn, 'on').andCallFake(function (evt, func) {
+        callback = func
+      });
+      spyOn(radioButtonsMock, 'markSelected');
+      radioButtonsMock.$elms.eq(0).attr('checked', true);
+      GOVUK.RadioButtons.prototype.bindEvents.call(radioButtonsMock);
+      callback({ 'target' : radioButtonsMock.$elms[0] });
+      expect(radioButtonsMock.markSelected).toHaveBeenCalled();
+    });
+  });
+
+  describe("markSelected method", function () {
+    it("Should add the selectedClass class to the label of the sent in radio", function () {
+      var radioButtonsMock = {
+            'selections' : {
+              'size' : false
+            },
+            'selectedClass' : 'selected'
+          },
+          $clickedRadio = $radioButtons.eq(0);
+
+      GOVUK.RadioButtons.prototype.markSelected.call(radioButtonsMock, $clickedRadio);
+      expect($clickedRadio.parent('label').hasClass('selected')).toEqual(true);
+    });
+
+    it("Should remove the selectedClass class from the label of the previously selected radio", function () {
+      var radioButtonsMock = {
+            'selections' : {
+              'size' : 'medium'
+            },
+            'selectedClass' : 'selected'
+          },
+          $clickedRadio = $radioButtons.eq(0);
+
+      $labels.eq(1).addClass('selected');
+      GOVUK.RadioButtons.prototype.markSelected.call(radioButtonsMock, $clickedRadio);
+      expect($('#medium').parent('label').hasClass('selected')).toEqual(false);
+
+    });
+  });
+
+  describe("setSelectedClass method", function () {
+    it("Should set the selectedClass on the instance to the string sent in", function () {
+      var radioButtonsMock = {};
+
+      GOVUK.RadioButtons.prototype.setSelectedClass.call(radioButtonsMock, 'selectable-selected');
+      expect(radioButtonsMock.selectedClass).toEqual('selectable-selected');
+    });
+  });
+});
