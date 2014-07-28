02eba610b28c35a493e517fccfb1a4f11ac1aa42
Tom Byers <tombaromba@gmail.com>
2014-07-28 14:18:56 +0100

    Clean up structure and fix loading of scripts
    There was no dependancy in the loading of each
    script of the previous one having loaded.

--- spec/support/load.js
+++ spec/support/load.js
@@ -2,20 +2,37 @@
   "use strict"
   var loadedScripts = 0,
       totalScripts,
+      merge,
       loadScript,
       runJasmine,
       manifestScript;
 
-  loadScript = function (src) {
-    var script = document.createElement('script');
+  merge = function (arrays) {
+    var resultingArray = [],
+        workingArray,
+        a, b, x, y;
+
+    for (a = 0, b = arrays.length; a < b; a++) {
+      workingArray = arrays[a];
+      for (x = 0, y = workingArray.length; x < y; x++) {
+        resultingArray.push(workingArray[x]);
+      }
+    }
+    return resultingArray;
+  };
+  loadScript = function (src, nextIdx) {
+    var script = document.createElement('script'),
+        nextScript; 
 
     script.type = 'text/javascript';
     script.src = src;
 
     document.getElementsByTagName('head')[0].appendChild(script);
+    if (nextIdx === undefined) { return script; }
     script.onload = function () {
-      loadedScripts++;
-      if (loadedScripts === totalScripts) {
+      if (nextIdx < totalScripts.length) {
+        loadScript(totalScripts[nextIdx], nextIdx + 1);
+      } else {
         runJasmine();
       }
     };
@@ -27,22 +44,12 @@
     jasmine.getEnv().addReporter(console_reporter);
     jasmine.getEnv().execute();
   };
-  manifestScript = loadScript('./manifest.js');
+  manifestScript = loadScript('../manifest.js');
 
   manifestScript.onload = function () {
-    var idx = 0,
-        supportFilesLength = manifest.support.length,
-        testFilesLength = manifest.test.length;
+    var idx = 0;
   
-    totalScripts = supportFilesLength + testFilesLength;
-    while (idx < supportFilesLength) {
-      loadScript(manifest.support[idx]);
-      idx++
-    }
-    idx = 0;
-    while (idx < testFilesLength) {
-      loadScript(manifest.test[idx]);
-      idx++
-    }
+    totalScripts = merge([manifest.support, manifest.test]);
+    loadScript(totalScripts[idx], idx + 1);
   };
 }).call(this);
