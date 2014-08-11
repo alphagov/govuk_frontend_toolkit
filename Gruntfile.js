10c757fa401018c328e4136e43915e3ba4f2b531
James Abley <james.abley@gmail.com>
2014-08-11 10:53:39 +0100

    Add basic Sass test to check our Sass compiles
    Slightly crufty initial implementation. The SCSS imports are static,
    rather than generating them at test time and so being resilient to
    future additions. I think that approach would require writing a grunt
    plugin, or switching to use gulp. Punted for now.
    
    Tested by reverting 31b973d11c968a5db5a4e4de5a19508eac932333 and running
    the tests. They failed with:
    
    ```
     > grunt
    Running "sass:development" (sass) task
    Syntax error: Positional arguments must come before keyword arguments.
            on line 42 of /Users/jabley/govuk/govuk_frontend_toolkit/stylesheets/_typography.scss
            from line 2 of /Users/jabley/govuk/govuk_frontend_toolkit/stylesheets/design-patterns/_alpha-beta.scss
            from line 6 of spec/stylesheets/test.scss
      Use --trace for backtrace.
    Warning: Exited with error code 1 Use --force to continue.
    
    Aborted due to warnings.
    ```

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 module.exports = function(grunt) {
   grunt.initConfig({
+    clean: {
+      sass: ["spec/stylesheets/test-out.css"]
+    },
     jasmine: {
       javascripts: {
         src: [
@@ -11,9 +14,24 @@ module.exports = function(grunt) {
           helpers: 'spec/unit/*Helper.js'
         }
       }
+    },
+    sass: {
+      development: {
+        files: {
+          'spec/stylesheets/test-out.css': 'spec/stylesheets/test.scss'
+        },
+        options: {
+          loadPath: [
+            'stylesheets'
+          ],
+          style: 'nested',
+        }
+      },
     }
   });
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jasmine');
-  grunt.registerTask('test', ['jasmine']);
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.registerTask('test', ['sass', 'clean', 'jasmine']);
   grunt.registerTask('default', ['test']);
 };
